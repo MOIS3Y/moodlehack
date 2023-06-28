a04799a89d458725b2fb5679afbd0bde0322cc2a
MOIS3Y <stepan@zhukovsky.me>
2023-06-28 16:29:46 +0900

    Add: answer model

--- moodlehack/answers/models.py
+++ moodlehack/answers/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Answer(models.Model):
+    question = models.TextField(unique=True)
+    answer = models.TextField()
+    url = models.URLField()
+    tag = models.CharField(max_length=50)
+    actual = models.BooleanField(default=True)
+    period = models.DateField(auto_now_add=True)
+    create = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.question
