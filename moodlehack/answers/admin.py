6e95a04287cca9c8099acedc2c801c3775bf044c
MOIS3Y <stepan@zhukovsky.me>
2023-06-29 23:28:35 +0900

    Update: admin site and 403 main template

--- moodlehack/answers/admin.py
+++ moodlehack/answers/admin.py
@@ -1,6 +1,37 @@
 from django.contrib import admin
-from .models import Answer
+from django.utils.html import format_html
+from .models import Category, Period, Answer
 
 
 # Register your models here.
-admin.site.register(Answer)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+
+class PeriodAdmin(admin.ModelAdmin):
+    list_display = ('id', 'period')
+    search_fields = ('period',)
+
+
+@admin.display(description="Click URL")
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'question', 'answer', 'source', 'actual', 'period', 'category'
+    )
+    # list_display_links = ()
+    search_fields = ('question', 'answer')
+    list_filter = ('category', 'period')
+
+    def source(self, obj):
+        return format_html(
+            "<a href='{url}' target='_blank'>{url}</a>", url=obj.url
+        )
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Period, PeriodAdmin)
+admin.site.register(Answer, AnswerAdmin)
+
+admin.site.site_title = 'Akvolabean'
+admin.site.site_header = 'AKVOLABEAN'
--- moodlehack/answers/models.py
+++ moodlehack/answers/models.py
@@ -1,16 +1,53 @@
 from django.db import models
-from month.models import MonthField
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=50, db_index=True, verbose_name='Категория'
+    )
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Period(models.Model):
+    period = models.DateField(verbose_name='Период')
+
+    class Meta:
+        verbose_name = 'Период'
+        verbose_name_plural = 'Периоды'
+        ordering = ['-period']
+
+    def __str__(self):
+        return str(self.period.strftime('%B - %Y'))
 
 
 class Answer(models.Model):
-    question = models.TextField(unique=True)
-    answer = models.TextField()
-    url = models.URLField()
-    tag = models.CharField(max_length=50)
-    actual = models.BooleanField(default=True)
-    period = MonthField("Period", help_text="month of testing")
+    question = models.TextField(unique=True, verbose_name='Вопрос')
+    answer = models.TextField(verbose_name='Ответ')
+    url = models.URLField(null=True, blank=True, verbose_name='Источник')
+    tag = models.CharField(
+        max_length=50, null=True, blank=True, verbose_name='Тег'
+    )
+    actual = models.BooleanField(default=True, verbose_name='Актуален')
     create = models.DateTimeField(auto_now_add=True)
     update = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(
+        'Category', on_delete=models.PROTECT, verbose_name='Категория'
+    )
+    period = models.ForeignKey(
+        'Period', on_delete=models.PROTECT, verbose_name='Период'
+    )
+
+    class Meta:
+        verbose_name = 'Ответы'
+        verbose_name_plural = 'Ответы'
+        ordering = ['update', 'period']
 
     def __str__(self):
         return self.question
