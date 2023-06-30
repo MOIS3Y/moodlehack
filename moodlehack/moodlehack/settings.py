f322c58fc164e46d483cee5725c8922735c5d469
MOIS3Y <stepan@zhukovsky.me>
2023-06-30 16:55:52 +0900

    Add: gunicorn to poetry deps

--- moodlehack/moodlehack/settings.py
+++ moodlehack/moodlehack/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'answers.apps.AnswersConfig'
+    'answers.apps.AnswersConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
