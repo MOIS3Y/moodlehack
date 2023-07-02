99099b5d4a2e4be47f273d42b9c0dffcb52e546f
MOIS3Y <stepan@zhukovsky.me>
2023-07-02 19:48:20 +0900

    Finally: ready to deploy as a Docker app

---
+++ README.md
@@ -0,0 +1,4 @@
+# MOODLEHACK
+
+Web application on Django-framework
+to collect ready-made answers to tests in the Moodle system.
--- moodlehack/moodlehack/settings.py
+++ moodlehack/moodlehack/settings.py
@@ -86,12 +86,6 @@ WSGI_APPLICATION = 'moodlehack.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': env.db_url(
         'SQLITE_URL',
@@ -135,11 +129,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = '/static/'
-
-# if DEBUG:
-#     STATIC_ROOT = BASE_DIR / 'static'
-# else:
-#     STATIC_ROOT = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
