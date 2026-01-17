540bd47139b7b2187ad0c94a73c959baa4753875
MOIS3Y <stepan@zhukovsky.me>
2026-01-17 15:00:57 +0900

    chore(urls): design for general code styling

--- moodlehack/moodlehack/urls.py
+++ moodlehack/moodlehack/urls.py
@@ -1,26 +1,24 @@
 from django.contrib import admin
-from django.urls import path, include
-
-from rest_framework.authtoken import views
+from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-
+from rest_framework.authtoken import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('answers.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("answers.urls")),
     path("accounts/", include("accounts.urls")),
 ]
 
 
 urlpatterns += [
-    # API PATTERNS
-    path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # API Patterns
+    path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
     # Optional UI:
     path(
-        'api/v1/schema/swagger/',
-        SpectacularSwaggerView.as_view(url_name='schema'),
-        name='swagger-ui'
+        "api/v1/schema/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui"
     ),
-    # API Token auth
-    path('api/v1/auth', views.obtain_auth_token)
+    # API Token Auth
+    path("api/v1/auth", views.obtain_auth_token)
 ]
